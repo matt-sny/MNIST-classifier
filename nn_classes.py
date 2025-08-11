314f8bcfd5c59879c7863b4b85e1e03a45ed1ded
Matthew <msnyman96@gmail.com>
2025-08-11 13:15:07 +0200

    corrected number of inputs to first fully connected layer

--- nn_classes.py
+++ nn_classes.py
@@ -6,7 +6,7 @@ class SimpleCNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc1 = nn.Linear(64 * 14 * 14, 128)
         self.fc2 = nn.Linear(128, 10)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.25)
